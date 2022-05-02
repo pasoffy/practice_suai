1db1b692361654161652c4b4efb16af922bde39e
Софья <Vodonok@gmail.com>
2022-05-02 12:05:57 +0300

    создание массива слов

--- main.py
+++ main.py
@@ -18,13 +18,31 @@ def is_it_empty(file):
     return file.readline() != '\n'
 
 
+def word_division(file, words):
+    input_file.seek(0)
+    for line in file:
+        words.extend(line.strip().split())
+
+
+def updating_words(words):
+    for i in range(len(words)):
+        word = words.pop(i)
+        punctuations = ['.', ',', '!', '?', '-', '%', ';', ':', '/', '"', "'"]
+        for mark in punctuations:
+            word = word.strip(mark)
+        words.insert(i, word)
+    return words
+
+
 name_of_file = input("Введите название файла или путь к нему: ")
 
 if check_file(name_of_file):
     with open(name_of_file, 'r', encoding='utf-8') as input_file:
 
         if is_it_empty(input_file):
-            print("its ok")
+            original_words = []
+            word_division(input_file, original_words)
+            update_words = updating_words(original_words)
         else:
             print("its not ok")
 
